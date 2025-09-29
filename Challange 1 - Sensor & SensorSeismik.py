b1fabbb0af7da2083607416bc8b85df7db88a94c
farelpriyarajendra <farel.124120018@student.itera.ac.id>
2025-09-29 21:05:37 +0700

    Update Challange 1 - Sensor & SensorSeismik.py

--- Challange 1 - Sensor & SensorSeismik.py
+++ Challange 1 - Sensor & SensorSeismik.py
@@ -6,3 +6,12 @@ class Sensor:
     
     def info(self):
         return f"Sensor ID: {self.id_sensor}, Lokasi: {self.lokasi}, Jenis: {self.jenis}"
+
+class SensorSeismik(Sensor):
+    def __init__(self, id_sensor, lokasi, jenis, frekuensi_sampling, data):
+        super().__init__(id_sensor, lokasi, jenis)
+        self.frekuensi_sampling = frekuensi_sampling
+        self.data = data
+    
+    def jumlah_sampel(self):
+        return len(self.data)
