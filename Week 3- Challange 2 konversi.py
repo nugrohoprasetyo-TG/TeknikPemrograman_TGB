3e93b1b8ae12fd26ae7da0004034bfdcd0e5b95e
verol124120099-ops <verol.124120099@student.itera.ac.id>
2025-12-16 20:06:19 +0700

    Week 3- Challange 2 konversi.py

---
+++ Week 3- Challange 2 konversi.py
@@ -0,0 +1,8 @@
+def c_to_f(c):
+    # Konversi Celcius ke Fahrenheit
+    return (c * 9/5) + 32
+
+def f_to_c(f):
+    # konversi Fahrenheit ke Celcius
+    return (f - 32) * 5/9
+ 65 changes: 65 additions & 0 deletions65  
